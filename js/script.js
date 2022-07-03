1b220e680a9874e3d295a76412255377816b68ca
ScienisTmiaoT <1730402302@qq.com>
2022-07-03 17:56:47 -0500

    implement infinite scroll to save network bandwidth (lazy loading for images)

--- js/script.js
+++ js/script.js
@@ -4,7 +4,13 @@ document.getElementById('datajs').src = 'js/data.js' + '?v=' + Date.now();
 const jsonData = JSON.parse(data);
 
 var parent = document.querySelector('.m-p-g__thumbs');
-for (let pic of jsonData["data"]) {
+
+const dataList = jsonData["data"];
+const MAX_SIZE = 6;
+let start = 0;
+start = appendImgs(parent, start, start + 2 * MAX_SIZE, dataList);
+
+function appendImg(pic, parent) {
     var img = document.createElement('img');
     img.src = "./img/" + pic["filename"];
     img.title = pic["title"] + "@" + pic["date"];
@@ -13,8 +19,26 @@ for (let pic of jsonData["data"]) {
     parent.appendChild(img);
 }
 
+function appendImgs(parent, start, end, dl) {
+  let e = Math.min(end, dl.length);
+  for (let i = start; i < e; i++) {
+    appendImg(dl[i], parent);
+  }
+  return e;
+}
+
 var elem = document.querySelector('.m-p-g');
 
 document.addEventListener('DOMContentLoaded', function () {
     var gallery = new MaterialPhotoGallery(elem);
 });
+
+window.addEventListener('scroll', () => {
+    if (window.scrollY + window.innerHeight >=
+      document.documentElement.scrollHeight) {
+      if (start < dataList.length) {
+        start = appendImgs(parent, start, start + MAX_SIZE, dataList);
+        var gallery = new MaterialPhotoGallery(document.querySelector('.m-p-g'));
+      }
+    }
+  });
