743accf8cf98266fdada125ab9003c5dfaedc7af
Wenbin Guo <ScienisTmiaoT@users.noreply.github.com>
2023-11-26 00:38:23 -0600

    bugfix

--- js/script.js
+++ js/script.js
@@ -2,59 +2,123 @@ var base_url = "https://raw.githubusercontent.com/ScienisTmiaoT/photogallery/mai
 var name_url = base_url + "/js/data.json" + '?v=' + Date.now();
 
 fetch(name_url).then(function (response) {
-  // The API call was successful!
-  if (response.ok) {
-      return response.json();
-  } else {
-      return Promise.reject(response);
-  }
+    // The API call was successful!
+    if (response.ok) {
+        return response.json();
+    } else {
+        return Promise.reject(response);
+    }
 }).then(function (data) {
-  // This is the JSON from our response
-  driver(data);
+    // This is the JSON from our response
+    driver(data);
 }).catch(function (err) {
-  // There was an error
-  console.warn('Something went wrong.', err);
+    // There was an error
+    console.warn('Something went wrong.', err);
 });
 
 function driver(jsonData) {
 
-var parent = document.querySelector('.m-p-g__thumbs');
+    var parent = document.querySelector('.gallery-container');
 
-const dataList = jsonData["data"];
-const MAX_SIZE = 6;
-let start = 0;
-start = appendImgs(parent, start, start + 2 * MAX_SIZE, dataList);
+    const dataList = jsonData["data"];
+    const MAX_SIZE = 6;
+    let start = 0;
+    start = appendImgs(parent, start, start + 2 * MAX_SIZE, dataList);
 
-function appendImg(pic, parent) {
-    var img = document.createElement('img');
-    img.src = "./img/" + pic["filename"];
-    img.title = pic["title"] + "@" + pic["date"];
-    img.setAttribute("data-full", "./img/" + pic["filename"]);
-    img.setAttribute("class", "m-p-g__thumbs-img");
-    parent.appendChild(img);
-}
+    init();
 
-function appendImgs(parent, start, end, dl) {
-  let e = Math.min(end, dl.length);
-  for (let i = start; i < e; i++) {
-    appendImg(dl[i], parent);
-  }
-  return e;
-}
-
-var elem = document.querySelector('.m-p-g');
-
-document.addEventListener('DOMContentLoaded', function () {
-    var gallery = new MaterialPhotoGallery(elem);
-});
-
-window.addEventListener('scroll', () => {
-    if (window.scrollY + window.innerHeight >=
-      document.documentElement.scrollHeight) {
-      if (start < dataList.length) {
-        start = appendImgs(parent, start, start + MAX_SIZE, dataList);
-        var gallery = new MaterialPhotoGallery(document.querySelector('.m-p-g'));
-      }
+    function appendImg(pic, parent, id) {
+        var div = document.createElement('div');
+        div.setAttribute('class', 'gallery-item');
+        div.setAttribute('data-index', id + 1);
+        var img = document.createElement('img');
+        img.src = "./img/" + pic["filename"];
+        img.title = pic["title"] + "@" + pic["date"];
+        div.appendChild(img);
+        parent.appendChild(div);
     }
-  });
+
+    function appendImgs(parent, start, end, dl) {
+        let e = Math.min(end, dl.length);
+        for (let i = start; i < e; i++) {
+            appendImg(dl[i], parent, i);
+        }
+        return e;
+    }
+
+
+    window.addEventListener('scroll', () => {
+        if (window.scrollY + window.innerHeight >=
+            document.documentElement.scrollHeight) {
+            if (start < dataList.length) {
+                start = appendImgs(parent, start, start + MAX_SIZE, dataList);
+                init();
+            }
+        }
+    });
+}
+
+
+function init() {
+    const galleryItem = document.getElementsByClassName("gallery-item");
+    const lightBoxContainer = document.createElement("div");
+    const lightBoxContent = document.createElement("div");
+    const lightBoxImg = document.createElement("img");
+    const lightBoxPrev = document.createElement("div");
+    const lightBoxNext = document.createElement("div");
+
+    lightBoxContainer.classList.add("lightbox");
+    lightBoxContent.classList.add("lightbox-content");
+    lightBoxPrev.classList.add("fa", "fa-angle-left", "lightbox-prev");
+    lightBoxNext.classList.add("fa", "fa-angle-right", "lightbox-next");
+
+    lightBoxContainer.appendChild(lightBoxContent);
+    lightBoxContent.appendChild(lightBoxImg);
+    lightBoxContent.appendChild(lightBoxPrev);
+    lightBoxContent.appendChild(lightBoxNext);
+
+    document.body.appendChild(lightBoxContainer);
+
+    let index = 1;
+
+    function showLightBox(n) {
+        if (n > galleryItem.length) {
+            index = 1;
+        } else if (n < 1) {
+            index = galleryItem.length;
+        }
+        let imageLocation = galleryItem[index - 1].children[0].getAttribute("src");
+        lightBoxImg.setAttribute("src", imageLocation);
+    }
+
+    function currentImage() {
+        lightBoxContainer.style.display = "block";
+
+        let imageIndex = parseInt(this.getAttribute("data-index"));
+        showLightBox(index = imageIndex);
+    }
+    for (let i = 0; i < galleryItem.length; i++) {
+        galleryItem[i].addEventListener("click", currentImage);
+    }
+
+    function slideImage(n) {
+        showLightBox(index += n);
+    }
+
+    function prevImage() {
+        slideImage(-1);
+    }
+
+    function nextImage() {
+        slideImage(1);
+    }
+    lightBoxPrev.addEventListener("click", prevImage);
+    lightBoxNext.addEventListener("click", nextImage);
+
+    function closeLightBox() {
+        if (this === event.target) {
+            lightBoxContainer.style.display = "none";
+        }
+    }
+    lightBoxContainer.addEventListener("click", closeLightBox);
 }
